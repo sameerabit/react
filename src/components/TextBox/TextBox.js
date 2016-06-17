393122fd478a0e6c4f5ff5f1544df355662ec77a
sameera <ijsecmjdabsd>
2016-06-17 14:27:41 +0530

    react starting of project

---
+++ src/components/TextBox/TextBox.js
@@ -0,0 +1,22 @@
+/**
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-2016 Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React, { Component } from 'react';
+
+class TextBox extends Component {
+
+  render() {
+    return (
+        <input type="text"/>
+    );
+  }
+
+}
+
+export default TextBox;
